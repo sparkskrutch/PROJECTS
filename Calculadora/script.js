d4ff0c73e8085af12666d46cb2358dfa87e5d8a7
sparkskruth <melissa.limaft@gmail.com>
2023-04-27 12:48:31 -0300

    criação html e estilização, sem java script

---
+++ Calculadora/script.js
@@ -0,0 +1,23 @@
+const button_1 = document.getElementById('button_1')
+const button_2 = document.getElementById('button_2');
+const display_insert = document.getElementById('display_insert');
+const number_0 = 0
+const number_1 = 1
+const number_2 = 2
+const number_3 = 3
+const number_4 = 4
+const number_5 = 5
+const number_6 = 6
+const number_7 = 7
+const number_8 = 8
+const number_9 = 9
+
+
+button_1.addEventListener('click', () => {
+    display_insert.innerText = number_1
+
+})
+
+button_2.addEventListener('click', () => {
+    display_insert.innerText = number_2
+})
